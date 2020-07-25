381c110f01feb2ffcdf48007261ca9fc60138d05
unknown <nole.anis@gmail.com>
2020-07-25 17:09:53 +0100

    Updates

---
+++ precache-manifest.f36a2c451a6bd73907da0b51144f6675.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4b30d7f075ae7ef9aecd2668af6c3a3",
+    "url": "/myportfolio/index.html"
+  },
+  {
+    "revision": "10fde861b9690d27dc7f",
+    "url": "/myportfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5d08c9d3669c64915aca",
+    "url": "/myportfolio/static/js/2.f22d3758.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myportfolio/static/js/2.f22d3758.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10fde861b9690d27dc7f",
+    "url": "/myportfolio/static/js/main.e4d65f14.chunk.js"
+  },
+  {
+    "revision": "da2e02db1e870a8bb8e5",
+    "url": "/myportfolio/static/js/runtime-main.b5f0a273.js"
+  }
+]);
